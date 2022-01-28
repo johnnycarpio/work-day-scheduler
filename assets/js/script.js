8efce52dbe7b55cb4937badb9dc326e08270e955
Johnny Carpio <johnnycarpio04@gmail.com>
2022-01-27 17:56:45 -0800

    initial project structure

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// use #currentDay to display current day and date
+
+var currentDate = Date();
+$("#currentDay").html(currentDate);
+
+// addeventlistener on save button to append text area value as HTML
+
+// addeventlistener on save button to save text area value and location to localStorage
+
+// depending on the time of day, use .addClass (classes in CSS) to color code
+
+// get values and locations from localStorage
